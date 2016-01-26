6bcc07a6d2a3ad72cda91924c97028f251ab8742
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-01-26 19:21:44 +0800

    Setting for parallel build worker

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    echo "usage: ./build.sh <number of parallel build worker>"
+    exit 1
+fi
+
 if [[ ! -d build ]]; then
     mkdir -v build
 fi
+
 cd build
 cmake ..
 cd ..
-cmake --build build --target nettomon
+cmake --build build --target nettomon -- -j $1
