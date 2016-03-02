a7a8e27f3bb2abaf496e81ba2489ce2c9d65981e
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-03-03 02:37:57 +0800

    Read proc net files only when a new socket is created

--- ProcReadTimer.cpp
+++ ProcReadTimer.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <future>
+#include <algorithm>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
@@ -14,7 +15,8 @@
 using namespace std;
 using namespace boost;
 
-void procRead(const system::error_code &code, asio::deadline_timer *timer, const string& pid);
+void procRead(const system::error_code &code, asio::deadline_timer *timer, const string &pid,
+              const vector<string>& cachedSocketsInode);
 
 ProcNetPublisher* procNetPublisher;
 
@@ -28,7 +30,7 @@ void ProcReadTimer::start(string pid) {
         asio::io_service io;
 
         asio::deadline_timer timer(io, posix_time::microseconds(0));
-        timer.async_wait(bind(procRead, asio::placeholders::error, &timer, processId));
+        timer.async_wait(bind(procRead, asio::placeholders::error, &timer, processId, vector<string>({""})));
 
         io.run();
     }, pid);
@@ -36,7 +38,8 @@ void ProcReadTimer::start(string pid) {
     procReadThread.detach();
 }
 
-void procRead(const system::error_code &code, asio::deadline_timer *timer, const string& pid) {
+void procRead(const system::error_code &code, asio::deadline_timer *timer, const string &pid,
+              const vector<string>& cachedSocketsInode) {
 
     Duration duration;
     duration.start();
@@ -47,49 +50,59 @@ void procRead(const system::error_code &code, asio::deadline_timer *timer, const
     unordered_map<string, NetData> tcp6InodeIp;
     unordered_map<string, NetData> udp6InodeIp;
 
-    #pragma omp parallel sections
-    {
-        #pragma omp section
-        socketsInode = ProcFd(pid).getSocketInodeList();
+    socketsInode = ProcFd(pid).getSocketInodeList();
 
-        #pragma omp section
-        tcpInodeIp = ProcNet("tcp").getInodesIpMap();
-
-        #pragma omp section
-        udpInodeIp = ProcNet("udp").getInodesIpMap();
-
-        #pragma omp section
-        tcp6InodeIp = ProcNet("tcp6").getInodesIpMap();
-
-        #pragma omp section
-        udp6InodeIp = ProcNet("udp6").getInodesIpMap();
+    int matchCounter = 0;
+    for (auto& cachedInode: cachedSocketsInode) {
+        auto iter = find(socketsInode.begin(), socketsInode.end(), cachedInode);
+        if (iter != socketsInode.end()) {
+            ++matchCounter;
+        }
     }
 
-    vector<NetData> tcpNetData;
-    vector<NetData> udpNetData;
-    vector<NetData> tcp6NetData;
-    vector<NetData> udp6NetData;
+    if (matchCounter != cachedSocketsInode.size()) {
 
-    #pragma omp parallel sections
-    {
-        #pragma omp section
-        tcpNetData = InodeIpHelper::filterProccessIp(socketsInode, tcpInodeIp);
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            tcpInodeIp = ProcNet("tcp").getInodesIpMap();
 
-        #pragma omp section
-        udpNetData = InodeIpHelper::filterProccessIp(socketsInode, udpInodeIp);
+            #pragma omp section
+            udpInodeIp = ProcNet("udp").getInodesIpMap();
 
-        #pragma omp section
-        tcp6NetData = InodeIpHelper::filterProccessIp(socketsInode, tcp6InodeIp);
+            #pragma omp section
+            tcp6InodeIp = ProcNet("tcp6").getInodesIpMap();
 
-        #pragma omp section
-        udp6NetData = InodeIpHelper::filterProccessIp(socketsInode, udp6InodeIp);
+            #pragma omp section
+            udp6InodeIp = ProcNet("udp6").getInodesIpMap();
+        }
+
+        vector<NetData> tcpNetData;
+        vector<NetData> udpNetData;
+        vector<NetData> tcp6NetData;
+        vector<NetData> udp6NetData;
+
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            tcpNetData = InodeIpHelper::filterProccessIp(socketsInode, tcpInodeIp);
+
+            #pragma omp section
+            udpNetData = InodeIpHelper::filterProccessIp(socketsInode, udpInodeIp);
+
+            #pragma omp section
+            tcp6NetData = InodeIpHelper::filterProccessIp(socketsInode, tcp6InodeIp);
+
+            #pragma omp section
+            udp6NetData = InodeIpHelper::filterProccessIp(socketsInode, udp6InodeIp);
+        }
+
+        procNetPublisher->setNetData(tcpNetData, udpNetData, tcp6NetData, udp6NetData);
+        procNetPublisher->notifyObservers();
     }
-
-    procNetPublisher->setNetData(tcpNetData, udpNetData, tcp6NetData, udp6NetData);
-    procNetPublisher->notifyObservers();
 
     duration.end();
 
     timer->expires_at(timer->expires_at() + posix_time::microseconds(500000 - duration.inMicroSeconds()));
-    timer->async_wait(bind(procRead, asio::placeholders::error, timer, pid));
+    timer->async_wait(bind(procRead, asio::placeholders::error, timer, pid, socketsInode));
 }
